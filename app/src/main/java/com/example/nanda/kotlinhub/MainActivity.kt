f1ebb004389f4fbd7fc9c6313cf75c9a1fc31b73
Chen Meng <madelinecc1201@gmail.com>
2018-10-12 10:17:27 +0800

    Test json file

--- app/src/main/java/com/example/nanda/kotlinhub/MainActivity.kt
+++ app/src/main/java/com/example/nanda/kotlinhub/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.nanda.kotlinhub
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            val intent = Intent(this, ActivityTestReadText::class.java)
+            startActivity(intent)
+        }
     }
 }
