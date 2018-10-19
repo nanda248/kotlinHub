e56233cc415f1821b80bec3390d08b93069703df
Chen Meng <madelinecc1201@gmail.com>
2018-10-19 10:06:01 +0800

    Json Parsing is done

--- app/src/main/java/com/example/nanda/kotlinhub/ActivityTestReadText.kt
+++ app/src/main/java/com/example/nanda/kotlinhub/ActivityTestReadText.kt
@@ -3,11 +3,7 @@ package com.example.nanda.kotlinhub
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
-import kotlinx.android.synthetic.main.activity_test_read_text.*
-import java.io.BufferedInputStream
-import java.io.BufferedReader
-import java.io.InputStream
-import java.io.InputStreamReader
+import android.widget.TextView
 
 class ActivityTestReadText : AppCompatActivity() {
 
@@ -15,29 +11,37 @@ class ActivityTestReadText : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_read_text)
 
-        val btn = findViewById<Button>(R.id.btn1)
-        btn.setOnClickListener {
-
-            val inputStream: InputStream = resources.openRawResource(R.raw.test)
-            val bis = BufferedInputStream(inputStream)
-            val br  = BufferedReader(InputStreamReader(bis))
-            val jsonData = StringBuffer()
-            var line: String?
-
-            do {
-                line = br.readLine()
-                if (line == null) {
-                    break
-                }
-                jsonData.append(line + "\n")
-
-            } while (true)
-
-            br.close()
-            bis.close()
-            var data = jsonData.toString()
-
-            tv1.text = data
+        val tv = findViewById<TextView>(R.id.tv1)
+        val btnGetJson = findViewById<Button>(R.id.btn1)
+        btnGetJson.setOnClickListener{
+            JSONLoading(this, "topic_1", tv).execute()
         }
+
     }
+
+    /*fun getJsonFile(){
+        val inputStream: InputStream = resources.openRawResource(R.raw.test)
+        val bis = BufferedInputStream(inputStream)
+        val br  = BufferedReader(InputStreamReader(bis))
+        val jsonData = StringBuffer()
+        var line: String?
+
+        do {
+            line = br.readLine()
+            if (line == null) {
+                break
+            }
+            jsonData.append(line + "\n")
+
+        } while (true)
+
+        br.close()
+        bis.close()
+        var data = jsonData.toString()
+
+    }
+
+    fun parseJsonData(){
+
+    }*/
 }
