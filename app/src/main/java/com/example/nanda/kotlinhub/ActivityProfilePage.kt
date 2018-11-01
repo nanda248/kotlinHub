028c3ce920c1641e01790f63db0bc526e64e4ec9
nanda248 <nanda.nda88@gmail.com>
2018-11-01 23:12:25 +0800

    use activity instead of fragment and remove drawer navigation on login success

---
+++ app/src/main/java/com/example/nanda/kotlinhub/ActivityProfilePage.kt
@@ -0,0 +1,12 @@
+package com.example.nanda.kotlinhub
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ActivityProfilePage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_page)
+    }
+}
