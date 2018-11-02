c635adb3d80cab3424df69d52c8cd422c8ab3abe
nanda248 <nanda.nda88@gmail.com>
2018-11-02 08:45:42 +0800

    popup window working

--- app/src/main/java/com/example/nanda/kotlinhub/activities/ActivityTopic.kt
+++ app/src/main/java/com/example/nanda/kotlinhub/activities/ActivityTopic.kt
@@ -1,12 +1,20 @@
 package com.example.nanda.kotlinhub.activities
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
-import android.widget.TextView
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.*
 import com.example.nanda.kotlinhub.R
 import com.example.nanda.kotlinhub.helper.JSONHelper
 import com.example.nanda.kotlinhub.helper.Section
+import kotlinx.android.synthetic.main.activity_home_page.*
 
 class ActivityTopic : AppCompatActivity() {
 
@@ -31,7 +39,10 @@ class ActivityTopic : AppCompatActivity() {
                 displaySection(topic[i])
                 i++
             }else{
-                finish()
+                val myIntent = Intent()
+                myIntent.putExtra("Continent","Asia")
+                setResult(Activity.RESULT_OK, myIntent)
+                showPopupModal()
             }
         }
     }
@@ -65,5 +76,58 @@ class ActivityTopic : AppCompatActivity() {
             tvCode.text = codeString
             tvCode.setBackgroundResource(R.drawable.code_border)
         }
+    }
+
+    fun showPopupModal() {
+        val inflater: LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.popup_layout, null)
+        val popupWindow = PopupWindow(view, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            popupWindow.elevation = 10.0F
+        }
+
+        // If API level 23 or higher then execute the code
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            // Create a new slide animation for popup window enter transition
+            val slideIn = Slide()
+            slideIn.slideEdge = Gravity.TOP
+            popupWindow.enterTransition = slideIn
+
+            // Slide animation for popup window exit transition
+            val slideOut = Slide()
+            slideOut.slideEdge = Gravity.RIGHT
+            popupWindow.exitTransition = slideOut
+
+        }
+
+        val tv = view.findViewById<TextView>(R.id.tv_poopup_text)
+        tv.setText("Congratulation! You have completed n Level.")
+        val btnPopup = view.findViewById<Button>(R.id.btn_popup_close)
+
+        // Set click listener for popup window's text view
+        tv.setOnClickListener{
+            // Change the text color of popup window's text view
+            tv.setTextColor(Color.CYAN)
+        }
+
+        // Set a click listener for popup's button widget
+        btnPopup.setOnClickListener{
+            // Dismiss the popup window
+            popupWindow.dismiss()
+        }
+
+        // Set a dismiss listener for popup window
+        popupWindow.setOnDismissListener {
+            Toast.makeText(applicationContext,"Good Job", Toast.LENGTH_SHORT).show()
+//            finish()
+        }
+
+        TransitionManager.beginDelayedTransition(home_layout)
+        popupWindow.showAtLocation(
+                home_layout,
+                Gravity.CENTER,
+                0,0
+        )
     }
 }
