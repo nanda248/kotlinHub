b88b6923d391c8ab1e2fbce39a1e004897dcb6e6
Chen Meng <madelinecc1201@gmail.com>
2018-11-04 00:27:34 +0800

    Add lock function

--- app/src/main/java/com/example/nanda/kotlinhub/activities/ActivityOOP.kt
+++ app/src/main/java/com/example/nanda/kotlinhub/activities/ActivityOOP.kt
@@ -1,16 +1,33 @@
 package com.example.nanda.kotlinhub.activities
 
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import android.widget.TextView
 import com.example.nanda.kotlinhub.R
+import com.example.nanda.kotlinhub.helper.UserDBHelper
+import kotlinx.android.synthetic.main.activity_home_page.*
 
 class ActivityOOP : AppCompatActivity() {
+
+    lateinit var userDBHelper: UserDBHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_oop)
+
+        userDBHelper = UserDBHelper(this)
+        val username = userDBHelper.getUsername()
+        var progress = userDBHelper.getProgress(username)
 
         val btnCO = findViewById<Button>(R.id.btn_topic16)
         val btnConstructor = findViewById<Button>(R.id.btn_topic17)
@@ -22,60 +39,140 @@ class ActivityOOP : AppCompatActivity() {
         val btnCompanion = findViewById<Button>(R.id.btn_topic23)
 
         btnCO.setOnClickListener {
-            val myIntent = Intent(this, ActivityTopic()::class.java)
-            myIntent.putExtra("topicFile", "topic_16")
-            myIntent.putExtra("topicNum", 16)
-            startActivity(myIntent)
+            if(progress<15) {
+                showPopupModal()
+            }else {
+                val myIntent = Intent(this, ActivityTopic()::class.java)
+                myIntent.putExtra("topicFile", "topic_16")
+                myIntent.putExtra("topicNum", 16)
+                startActivity(myIntent)
+            }
         }
 
         btnConstructor.setOnClickListener {
-            val myIntent = Intent(this, ActivityTopic()::class.java)
-            myIntent.putExtra("topicFile", "topic_17")
-            myIntent.putExtra("topicNum", 17)
-            startActivity(myIntent)
+            if(progress<16) {
+                showPopupModal()
+            }else {
+                val myIntent = Intent(this, ActivityTopic()::class.java)
+                myIntent.putExtra("topicFile", "topic_17")
+                myIntent.putExtra("topicNum", 17)
+                startActivity(myIntent)
+            }
         }
 
         btnInheritance.setOnClickListener {
-            val myIntent = Intent(this, ActivityTopic()::class.java)
-            myIntent.putExtra("topicFile", "topic_18")
-            myIntent.putExtra("topicNum", 18)
-            startActivity(myIntent)
+            if(progress<17) {
+                showPopupModal()
+            }else {
+                val myIntent = Intent(this, ActivityTopic()::class.java)
+                myIntent.putExtra("topicFile", "topic_18")
+                myIntent.putExtra("topicNum", 18)
+                startActivity(myIntent)
+            }
         }
 
         btnAbstract.setOnClickListener {
-            val myIntent = Intent(this, ActivityTopic()::class.java)
-            myIntent.putExtra("topicFile", "topic_19")
-            myIntent.putExtra("topicNum", 19)
-            startActivity(myIntent)
+            if(progress<18) {
+                showPopupModal()
+            }else {
+                val myIntent = Intent(this, ActivityTopic()::class.java)
+                myIntent.putExtra("topicFile", "topic_19")
+                myIntent.putExtra("topicNum", 19)
+                startActivity(myIntent)
+            }
         }
 
         btnInterface.setOnClickListener {
-            val myIntent = Intent(this, ActivityTopic()::class.java)
-            myIntent.putExtra("topicFile", "topic_20")
-            myIntent.putExtra("topicNum", 20)
-            startActivity(myIntent)
+            if(progress<19) {
+                showPopupModal()
+            }else {
+                val myIntent = Intent(this, ActivityTopic()::class.java)
+                myIntent.putExtra("topicFile", "topic_20")
+                myIntent.putExtra("topicNum", 20)
+                startActivity(myIntent)
+            }
         }
 
         btnData.setOnClickListener {
-            val myIntent = Intent(this, ActivityTopic()::class.java)
-            myIntent.putExtra("topicFile", "topic_21")
-            myIntent.putExtra("topicNum", 21)
-            startActivity(myIntent)
+            if(progress<20) {
+                showPopupModal()
+            }else {
+                val myIntent = Intent(this, ActivityTopic()::class.java)
+                myIntent.putExtra("topicFile", "topic_21")
+                myIntent.putExtra("topicNum", 21)
+                startActivity(myIntent)
+            }
         }
 
         btnSealed.setOnClickListener {
-            val myIntent = Intent(this, ActivityTopic()::class.java)
-            myIntent.putExtra("topicFile", "topic_22")
-            myIntent.putExtra("topicNum", 22)
-            startActivity(myIntent)
+            if(progress<21) {
+                showPopupModal()
+            }else {
+                val myIntent = Intent(this, ActivityTopic()::class.java)
+                myIntent.putExtra("topicFile", "topic_22")
+                myIntent.putExtra("topicNum", 22)
+                startActivity(myIntent)
+            }
         }
 
         btnCompanion.setOnClickListener {
-            val myIntent = Intent(this, ActivityTopic()::class.java)
-            myIntent.putExtra("topicFile", "topic_23")
-            myIntent.putExtra("topicNum", 23)
-            startActivity(myIntent)
+            if(progress<22) {
+                showPopupModal()
+            }else {
+                val myIntent = Intent(this, ActivityTopic()::class.java)
+                myIntent.putExtra("topicFile", "topic_23")
+                myIntent.putExtra("topicNum", 23)
+                startActivity(myIntent)
+            }
         }
 
+    }
+
+    fun showPopupModal() {
+        val inflater: LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.popup_layout_locked, null)
+        val popupWindow = PopupWindow(view, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            popupWindow.elevation = 10.0F
+        }
+
+        // If API level 23 or higher then execute the code
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            // Create a new slide animation for popup window enter transition
+            val slideIn = Slide()
+            slideIn.slideEdge = Gravity.TOP
+            popupWindow.enterTransition = slideIn
+
+            // Slide animation for popup window exit transition
+            val slideOut = Slide()
+            slideOut.slideEdge = Gravity.RIGHT
+            popupWindow.exitTransition = slideOut
+
+        }
+
+        val tv = view.findViewById<TextView>(R.id.tv_poopup_text)
+        tv.setText("Complete the previous modules to unlock this one!")
+        val btnPopup = view.findViewById<Button>(R.id.btn_ok)
+
+        // Set a click listener for popup's button widget
+        btnPopup.setOnClickListener{
+            // Dismiss the popup window
+            popupWindow.dismiss()
+        }
+
+        // Set a dismiss listener for popup window
+        /*
+        popupWindow.setOnDismissListener {
+            Toast.makeText(applicationContext,"Good Job",Toast.LENGTH_SHORT).show()
+        }
+        */
+
+        TransitionManager.beginDelayedTransition(home_layout)
+        popupWindow.showAtLocation(
+                home_layout,
+                Gravity.CENTER,
+                0,0
+        )
     }
 }
